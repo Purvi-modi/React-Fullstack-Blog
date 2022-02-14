ea8d2da38bb7d05e448a287986f39765e9a89da5
Grokr-Purvi <purvi.modi@datagrokr.com>
2022-02-14 16:48:18 +0530

    Signup - added user registration, data validation, password confirmation

---
+++ src/components/Signup/index.jsx
@@ -0,0 +1,132 @@
+import React from "react";
+import { validateAll } from "indicative";
+
+class Signup extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: "",
+      password: "",
+      passowrd_confirmation: "",
+      email: "",
+      errors: {},
+    };
+  }
+
+  handleInputChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log(this.state);
+
+    // validating data
+
+    const data = this.state;
+
+    const rules = {
+      name: "required|string",
+      email: "required|email",
+      password: "required|string|min:4|confirmed",
+    };
+
+    validateAll(data, rules)
+      .then(() => {
+        console.log("SUCCESS");
+      })
+      .catch((errors) => {
+        console.log({ errors });
+
+        const formattedErrors = {};
+
+        errors.forEach(
+          (error) => (formattedErrors[error.field] = error.message)
+        );
+
+        this.setState({
+          errors: formattedErrors,
+        });
+      });
+  };
+  render() {
+    return (
+      <div
+        className="mh-fullscreen bg-img center-vh p-20"
+        style={{ backgroundImage: "url(assets/img/bg-girl.jpg)" }}
+      >
+        <div
+          className="card card-shadowed p-50 w-400 mb-0"
+          style={{ maxWidth: "100%" }}
+        >
+          <h5 className="text-uppercase text-center">Register</h5>
+          <br />
+          <br />
+          <form className="form-type-material" onSubmit={this.handleSubmit}>
+            <div className="form-group">
+              <input
+                type="text"
+                name="name"
+                onChange={this.handleInputChange}
+                className="form-control"
+                placeholder="Username"
+              />
+              <small className="text-danger">{this.state.errors["name"]}</small>
+            </div>
+            <div className="form-group">
+              <input
+                type="text"
+                name="email"
+                onChange={this.handleInputChange}
+                className="form-control"
+                placeholder="Email address"
+              />
+              <small className="text-danger">
+                {this.state.errors["email"]}
+              </small>
+            </div>
+            <div className="form-group">
+              <input
+                type="password"
+                name="password"
+                onChange={this.handleInputChange}
+                className="form-control"
+                placeholder="Password"
+              />
+              <small className="text-danger">
+                {this.state.errors["password"]}
+              </small>
+            </div>
+            <div className="form-group">
+              <input
+                type="password"
+                name="password_confirmation"
+                onChange={this.handleInputChange}
+                className="form-control"
+                placeholder="Password (confirm)"
+              />
+            </div>
+            <br />
+            <button
+              className="btn btn-bold btn-block btn-primary"
+              type="submit"
+            >
+              Register
+            </button>
+          </form>
+          <hr className="w-30" />
+          <p className="text-center text-muted fs-13 mt-20">
+            Already have an account?
+            <a href="login.html">Sign in</a>
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Signup;
